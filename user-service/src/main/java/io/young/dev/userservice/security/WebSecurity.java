86a886e04e85d76f9429b1297bee4a7c0e591a3b
Youngwook-Jeon <ela87@naver.com>
2022-02-01 16:27:31 +0900

    Add a filter to authenticate

--- user-service/src/main/java/io/young/dev/userservice/security/WebSecurity.java
+++ user-service/src/main/java/io/young/dev/userservice/security/WebSecurity.java
@@ -5,6 +5,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.servlet.Filter;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
@@ -12,7 +14,19 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeHttpRequests().antMatchers("/users/**").permitAll();
+//        http.authorizeHttpRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/**")
+                .hasIpAddress("218.154.30.204")
+                .and()
+                .addFilter(getAuthenticationFilter());
+
         http.headers().frameOptions().disable();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return authenticationFilter;
     }
 }
